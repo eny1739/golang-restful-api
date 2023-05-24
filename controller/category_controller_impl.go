3e306d3c1df130e33a98fc8c6b46272729dee2ee
eny <61767395+eny1739@users.noreply.github.com>
2023-05-24 08:07:19 +0700

    create controller for create

---
+++ controller/category_controller_impl.go
@@ -0,0 +1,54 @@
+package controller
+
+import (
+	"encoding/json"
+	"github.com/julienschmidt/httprouter"
+	"golang-restful-api/helper"
+	"golang-restful-api/model/web"
+	"golang-restful-api/service"
+	"net/http"
+)
+
+type CategoryControllerImpl struct {
+	CategoryService service.CategoryService
+}
+
+func (controller *CategoryControllerImpl) Create(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	//TODO implement me
+	decoder := json.NewDecoder(request.Body)
+	categoryCreateRequest := web.CategoryCreateRequest{}
+	err := decoder.Decode(&categoryCreateRequest)
+	helper.PanicIfError(err)
+
+	categoryResponse := controller.CategoryService.Create(request.Context(), categoryCreateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   categoryResponse,
+	}
+
+	writer.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(writer)
+	err = encoder.Encode(webResponse)
+	helper.PanicIfError(err)
+}
+
+func (controller *CategoryControllerImpl) Update(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (controller *CategoryControllerImpl) Delete(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (controller *CategoryControllerImpl) FindById(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (controller *CategoryControllerImpl) FindAll(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
+	//TODO implement me
+	panic("implement me")
+}
