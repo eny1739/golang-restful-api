37c79de02eaa52d8d6aec669fdb3d0104bf8e686
eny <61767395+eny1739@users.noreply.github.com>
2023-05-25 14:55:15 +0700

    create controller for update

--- controller/category_controller_impl.go
+++ controller/category_controller_impl.go
@@ -7,6 +7,7 @@ import (
 	"golang-restful-api/model/web"
 	"golang-restful-api/service"
 	"net/http"
+	"strconv"
 )
 
 type CategoryControllerImpl struct {
@@ -35,7 +36,28 @@ func (controller *CategoryControllerImpl) Create(writer http.ResponseWriter, req
 
 func (controller *CategoryControllerImpl) Update(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
 	//TODO implement me
-	panic("implement me")
+	decoder := json.NewDecoder(request.Body)
+	categoryUpdateRequest := web.CategoryUpdateRequest{}
+	err := decoder.Decode(&categoryUpdateRequest)
+	helper.PanicIfError(err)
+
+	categoryId := params.ByName("categoryId")
+	id, err := strconv.Atoi(categoryId)
+	helper.PanicIfError(err)
+
+	categoryUpdateRequest.Id = id
+
+	categoryResponse := controller.CategoryService.Update(request.Context(), categoryUpdateRequest)
+	webResponse := web.WebResponse{
+		Code:   200,
+		Status: "OK",
+		Data:   categoryResponse,
+	}
+
+	writer.Header().Add("Content-Type", "application/json")
+	encoder := json.NewEncoder(writer)
+	err = encoder.Encode(webResponse)
+	helper.PanicIfError(err)
 }
 
 func (controller *CategoryControllerImpl) Delete(writer http.ResponseWriter, request *http.Request, params httprouter.Params) {
